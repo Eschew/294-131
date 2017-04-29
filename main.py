b74ddaf70b9fe66f81d06bc5ee3b699cf10a8b07
Andrew <ahliu@berkeley.edu>
2017-04-28 22:11:20 -0700

    test Faster RCNN on youtube BB

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import os, sys, cv2
 import argparse
 from networks.factory import get_network
 
+os.putenv('CUDA_VISIBLE_DEVICES', '1')
+
 
 CLASSES = ('__background__',
            'aeroplane', 'bicycle', 'bird', 'boat',
@@ -17,42 +19,14 @@ CLASSES = ('__background__',
            'cow', 'diningtable', 'dog', 'horse',
            'motorbike', 'person', 'pottedplant',
            'sheep', 'sofa', 'train', 'tvmonitor')
-MODEL_FILE = '~/home/ahliu/Faster-RCNN_TF/VGGnet_fast_rcnn_iter_70000.ckpt'
-IM_FILES = ['000456.jpg', '000542.jpg', '001150.jpg',
-                '001763.jpg', '004545.jpg']
-IM_FOLDER = '/data/efros/ahliu/yt-bb2/'
-IM_FILES = ['/data/efros/ahliu/yt-bb2/ \
-        ca899NyehXE=14000=person=0.183=0.734=0.051666666=1.jpg']
 
+FRCN_ROOT = '/home/ahliu/Faster-RCNN_TF/'
+MODEL_FILE = FRCN_ROOT + 'VGGnet_fast_rcnn_iter_70000.ckpt'
 
-def vis_detections(im, class_name, dets,ax, thresh=0.5):
-    """Draw detected bounding boxes."""
-    inds = np.where(dets[:, -1] >= thresh)[0]
-    if len(inds) == 0:
-        return
+IM_ROOT = '/data/efros/ahliu/yt-bb2/'
+IM_FILES = ['ca899NyehXE=14000=person=0.183=0.734=0.051666666=1.jpg']
 
-    for i in inds:
-        bbox = dets[i, :4]
-        score = dets[i, -1]
-
-        ax.add_patch(
-            plt.Rectangle((bbox[0], bbox[1]),
-                          bbox[2] - bbox[0],
-                          bbox[3] - bbox[1], fill=False,
-                          edgecolor='red', linewidth=3.5)
-            )
-        ax.text(bbox[0], bbox[1] - 2,
-                '{:s} {:.3f}'.format(class_name, score),
-                bbox=dict(facecolor='blue', alpha=0.5),
-                fontsize=14, color='white')
-
-    ax.set_title(('{} detections with '
-                  'p({} | box) >= {:.1f}').format(class_name, class_name,
-                                                  thresh),
-                  fontsize=14)
-    plt.axis('off')
-    plt.tight_layout()
-    plt.draw()
+OUTPUT_ROOT = FRCN_ROOT + '294-131/output/'
 
 if __name__ == '__main__':
     cfg.TEST.HAS_RPN = True  # Use RPN for proposals
@@ -70,7 +44,7 @@ if __name__ == '__main__':
 
     for im_file in IM_FILES:
         print(im_file)
-        im = cv2.imread(im_file)
+        im = cv2.imread(os.path.join(IM_ROOT, im_file))
 
         # Detect all object classes and regress object bounds
         timer = Timer()
@@ -79,23 +53,6 @@ if __name__ == '__main__':
         timer.toc()
         print ('Detection took {:.3f}s for '
                '{:d} object proposals').format(timer.total_time, boxes.shape[0])
-
-        # Visualize detections for each class
-        im = im[:, :, (2, 1, 0)]
-        fig, ax = plt.subplots(figsize=(12, 12))
-        ax.imshow(im, aspect='equal')
-
-        CONF_THRESH = 0.8
-        NMS_THRESH = 0.3
-        for cls_ind, cls in enumerate(CLASSES[1:]):
-            cls_ind += 1 # because we skipped background
-            cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
-            cls_scores = scores[:, cls_ind]
-            dets = np.hstack((cls_boxes,
-                              cls_scores[:, np.newaxis])).astype(np.float32)
-            keep = nms(dets, NMS_THRESH)
-            dets = dets[keep, :]
-            vis_detections(im, cls, dets, ax, thresh=CONF_THRESH)
-
-    plt.show()
-
+        
+        np.save(os.path.join(OUTPUT_ROOT, 'scores', im_file), scores)
+        np.save(os.path.join(OUTPUT_ROOT, 'boxes', im_file), scores)
---
+++ visualize.py
@@ -0,0 +1,85 @@
+import _init_paths
+import tensorflow as tf
+from fast_rcnn.config import cfg
+from fast_rcnn.test import im_detect
+from fast_rcnn.nms_wrapper import nms
+from utils.timer import Timer
+import matplotlib.pyplot as plt
+import numpy as np
+import os, sys, cv2
+import argparse
+from networks.factory import get_network
+
+os.putenv('CUDA_VISIBLE_DEVICES', '1')
+
+
+CLASSES = ('__background__',
+           'aeroplane', 'bicycle', 'bird', 'boat',
+           'bottle', 'bus', 'car', 'cat', 'chair',
+           'cow', 'diningtable', 'dog', 'horse',
+           'motorbike', 'person', 'pottedplant',
+           'sheep', 'sofa', 'train', 'tvmonitor')
+
+FRCN_ROOT = '/home/ahliu/Faster-RCNN_TF/'
+MODEL_FILE = FRCN_ROOT + 'VGGnet_fast_rcnn_iter_70000.ckpt'
+
+IM_ROOT = '/data/efros/ahliu/yt-bb2/'
+IM_FILES = ['ca899NyehXE=14000=person=0.183=0.734=0.051666666=1.jpg']
+
+OUTPUT_ROOT = FRCN_ROOT + '294-131/output/'
+
+def vis_detections(im, class_name, dets,ax, thresh=0.5):
+    """Draw detected bounding boxes."""
+    inds = np.where(dets[:, -1] >= thresh)[0]
+    if len(inds) == 0:
+        return
+
+    for i in inds:
+        bbox = dets[i, :4]
+        score = dets[i, -1]
+
+        ax.add_patch(
+            plt.Rectangle((bbox[0], bbox[1]),
+                          bbox[2] - bbox[0],
+                          bbox[3] - bbox[1], fill=False,
+                          edgecolor='red', linewidth=3.5)
+            )
+        ax.text(bbox[0], bbox[1] - 2,
+                '{:s} {:.3f}'.format(class_name, score),
+                bbox=dict(facecolor='blue', alpha=0.5),
+                fontsize=14, color='white')
+
+    ax.set_title(('{} detections with '
+                  'p({} | box) >= {:.1f}').format(class_name, class_name,
+                                                  thresh),
+                  fontsize=14)
+    plt.axis('off')
+    plt.tight_layout()
+    plt.draw()
+
+if __name__ == '__main__':
+    cfg.TEST.HAS_RPN = True  # Use RPN for proposals
+       
+    for im_file in IM_FILES:
+        np.load(os.path.join(OUTPUT_ROOT, 'scores', im_file), scores)
+        np.load(os.path.join(OUTPUT_ROOT, 'boxes', im_file), scores)
+        
+        # Visualize detections for each class
+        im = im[:, :, (2, 1, 0)]
+        fig, ax = plt.subplots(figsize=(12, 12))
+        ax.imshow(im, aspect='equal')
+
+        CONF_THRESH = 0.8
+        NMS_THRESH = 0.3
+        for cls_ind, cls in enumerate(CLASSES[1:]):
+            cls_ind += 1 # because we skipped background
+            cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
+            cls_scores = scores[:, cls_ind]
+            dets = np.hstack((cls_boxes,
+                              cls_scores[:, np.newaxis])).astype(np.float32)
+            keep = nms(dets, NMS_THRESH)
+            dets = dets[keep, :]
+            vis_detections(im, cls, dets, ax, thresh=CONF_THRESH)
+ 
+    plt.show()
+
