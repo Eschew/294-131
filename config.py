88999526685e5d368af6525a4c212296e745f666
xinyutan17 <xinyutan@berkeley.edu>
2017-04-29 21:03:42 -0700

    allow local home dirs

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 
+from config_locals import HOME_DIR
+
 CLASSES = ('__background__',
            'aeroplane', 'bicycle', 'bird', 'boat',
            'bottle', 'bus', 'car', 'cat', 'chair',
@@ -7,13 +9,10 @@ CLASSES = ('__background__',
            'motorbike', 'person', 'pottedplant',
            'sheep', 'sofa', 'train', 'tvmonitor')
 
-# HOME_DIR = '/home/ahliu/294-131'
-HOME_DIR = '/mnt/c/Users/Dennis/Dropbox/UC Berkeley/Classes/Spring 2017/CS 294-131/project/'
 FRCN_ROOT = os.path.join(HOME_DIR, 'Faster-RCNN_TF')
 MODEL_FILE = FRCN_ROOT + 'VGGnet_fast_rcnn_iter_70000.ckpt'
 
 PROJECT_ROOT = os.path.join(HOME_DIR, '294-131')
-# INPUT_ROOT = '/data/efros/ahliu/yt-bb2/'
 INPUT_ROOT = os.path.join(PROJECT_ROOT, 'input')
 OUTPUT_ROOT = os.path.join(PROJECT_ROOT, 'output')
 
